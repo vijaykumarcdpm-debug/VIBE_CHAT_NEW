178af564697619b1bc68b975d36998a5bddc1430
vijaykumarcdpm-debug <vijaykumarcdpm@gmail.com>
2026-06-14 21:25:51 +0530

    Fix rejoin token restore for profile loading

--- remix_-vibechat (18)/src/App.tsx
+++ remix_-vibechat (18)/src/App.tsx
@@ -1490,8 +1490,9 @@ return true;
       fetchLatestProfile();
     }, 500);
   }
-}}
+           }}
 />
+        )
       ) : (
         <div className="flex-1 min-h-0 flex flex-col justify-between w-full max-w-full">
           
